85140b979ce429179842e255f6ee119ef02f1182
evgeniymarianov <evgeniymarianov@gmail.com>
2020-05-23 15:09:03 +0300

    Добавил стоимости виртуальным машинам и их дискам

--- vm_read.py
+++ vm_read.py
@@ -1,4 +1,5 @@
 import csv
+from functools import reduce
 
 
 with open('02_ruby_vms.csv') as f:
@@ -6,8 +7,12 @@ with open('02_ruby_vms.csv') as f:
     #print(vms_csv)
 
 with open('02_ruby_prices.csv') as f:
-    vms_prices = dict(csv.reader(f))
-    #print(vms_prices)
+    prices = dict(csv.reader(f))
+
+for key, value in prices.items():
+    if key in ['cpu', 'ram', 'hdd_capacity', 'ssd', 'sata', 'sas']:
+        prices[key] = int(value)
+print(prices)
         
 with open('02_ruby_volumes.csv') as f:
     vms_volumes = list(csv.reader(f))
@@ -18,41 +23,41 @@ with open('02_ruby_volumes.csv') as f:
 class VirtualMachine:
 
 	
-	def __init__(self, id, ram, cpu, hdd_type, hdd_capacity, add_hdds):
-		self.id = id
-		self.ram = ram 
-		self.cpu = cpu
+	def set(self, id, ram, cpu, hdd_type, hdd_capacity, add_hdds):
+		self.id = int(id)
+		self.ram = int(ram)
+		self.cpu = int(cpu)
 		self.hdd_type = hdd_type
-		self.hdd_capacity = hdd_capacity
-		self.add_hdds = []
-
-def create_vm(vm):
-	new_vm = VirtualMachine (
-		id = vm[0],
-		ram =  vm[1],
-    	cpu = vm[2],
-    	hdd_type = vm[3],
-    	hdd_capacity = vm[4],
-		add_hdds = [])	
-
-for vm in vms_csv:
-	create_vm(vm)
+		self.hdd_capacity = int(hdd_capacity)
+		self.add_hdds = add_hdds
+		self.cost = (cpu * prices['cpu'] + ram * prices['ram'] + hdd_capacity * prices[hdd_type] + sum(list(map(lambda hdd: hdd.cost, add_hdds)))) * 0.01
 
 class AdditionalHdd:
 
 	
-	def __init__(self, vm_id, hdd_type, hdd_capacity):
-		self.vm_id = vm_id
+	def set(self, vm_id, hdd_type, hdd_capacity):
+		self.vm_id = int(vm_id)
 		self.hdd_type = hdd_type
-		self.hdd_capacity = hdd_capacity
+		self.hdd_capacity = int(hdd_capacity)
+		self.cost = int(hdd_capacity) * prices[hdd_type]
 
-def create_hdd(hdd):
-	new_hdd = AdditionalHdd (
-		vm_id = hdd[0],
-    	hdd_type = hdd[1],
-    	hdd_capacity = hdd[2])
-
-print(vms_csv[0])
-
+AddHdds = []
 for hdd in vms_volumes:
-	create_hdd(hdd)
+	new_hdd = AdditionalHdd ()
+	new_hdd.set (hdd[0], hdd[1], hdd[2])
+	AddHdds.append(new_hdd)
+
+VMs = []
+for vm in vms_csv:
+	#print(vm)
+	new_vm = VirtualMachine ()
+	#print(list(filter(lambda hdd: hdd.vm_id == int(vm[0]), AddHdds)))
+	new_vm.set (int(vm[0]), int(vm[1]), int(vm[2]), vm[3], int(vm[4]), list(filter(lambda hdd: hdd.vm_id == int(vm[0]), AddHdds)))
+	VMs.append(new_vm)
+	print(new_vm.cost)
+
+
+
+
+
+
